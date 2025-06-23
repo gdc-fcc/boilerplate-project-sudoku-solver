e2c0101a799a9c724c0b3421f3f77321ab52c88d
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-23 07:31:59 +0200

    add empty tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,16 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver;
 
 suite('Unit Tests', () => {
-
+    test('Logic handles a valid puzzle string of 81 characters')
+    test('Logic handles a puzzle string with invalid characters (not 1-9 or .)')
+    test('Logic handles a puzzle string that is not 81 characters in length')
+    test('Logic handles a valid row placement')
+    test('Logic handles an invalid row placement')
+    test('Logic handles a valid column placement')
+    test('Logic handles an invalid column placement')
+    test('Logic handles a valid region (3x3 grid) placement')
+    test('Logic handles an invalid region (3x3 grid) placement')
+    test('Valid puzzle strings pass the solver')
+    test('Invalid puzzle strings fail the solver')
+    test('Solver returns the expected solution for an incomplete puzzle')
 });
